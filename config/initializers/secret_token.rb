f78e79826db93183ad251f17070e81c2f7cd3178
Casey Siewert <caseysiewert@gmail.com>
2013-07-22 21:07:18 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cdwedding::Application.config.secret_token = '1660305ec0057fd8ff5fae6245dab750661343c867a4e231198122e217f6e283f3a6013dc697bc6d9ee15855e39ce34870613f6fbe863412afa6cfbdec2aaf2c'
